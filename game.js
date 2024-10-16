8d3781e0d5041ddf2b80c476b2d42a8b294e24f8
Lemnsa <legeryennyuy@gmail.com>
2024-10-16 07:07:13 +0100

    Update Boxes With Players Marks. Get Winner and Check Tie Game.

--- game.js
+++ game.js
@@ -5,11 +5,18 @@ const playersLis = document.querySelectorAll('.players .player');
 const addNameBtn = document.querySelector('button');
 
 
-let innitialBoard = [
-  ['','',''],
-  ['','',''],
-  ['','','']
-];
+let innitialBoard = [...grids];
+
+const winningCombinations = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+]
 
 console.log(innitialBoard)
 
@@ -31,37 +38,19 @@ const currentPlayer = () => {
 }
 
 
-const playRound = (activePlayer, row, col) => {
-
-  if (innitialBoard[row][col] !== '') {
-    return;
-}
-innitialBoard[row][col] = activePlayer.marker;
-switchRounds(activePlayer);
-return innitialBoard;
-
-}
-
-
-const switchRounds = (currentPlayer) => {
+function switchRounds  (currentPlayer)  {
 
   return (currentPlayer.marker === 'o') ? playerTwo :  playerOne;
    
 }
 
 
-const checkWinner = (board, playerMarker) => {
-  for (let combination of winningCombinnations) {
-    let win = true;
-    for (let [row, col] of combination) {
-      if(board[row][col] !== playerMarker) {
-        win = false;
-        break;
-      }
-    }
-    if(win) {
-      return `player with ${playerMarker} won`;
-    }
+const checkWinner = (playerMarker) => {
+  for(let i = 0; i < winningCombinations.length; i++) {
+    const [a,b,c] = winningCombinations[i];
+    if(grids[a].textContent === playerMarker && grids[b].textContent === playerMarker && grids[c].textContent === playerMarker){
+      return true;
+  }
   }
   return false;
 }
@@ -69,24 +58,67 @@ const checkWinner = (board, playerMarker) => {
 
 
 function isArrayFull(board) {
-  for (let row; row < board.length; row++) {
-    for (let col; col < board[row].length; col++) {
-      if(board[row][col] === '') {
+  for (let i = 0; i < board.length; i++) {
+      if(board[i].textContent ==='') {
         return false;
       }
-    }
   }
   return true; // no empty cell was found
 }
 
 
-let updateDiv = (player, divs) => {
-  divs.forEach(div => {
-    div.addEventListener("click", () => {
-      if (div.className !== 'grid') return;
-      div.textContent = player;
-    })
-  });
-}
+// let updateDiv = (player, divs) => {
+//   divs.forEach(div => {
+      
+//       div.addEventListener("click", () => {
+//         switchRounds(currentPlayer().activePlayer);
+//         if (!isArrayFull(innitialBoard)) {
+//           if (div.className !== 'grid') return;
+//         div.textContent = player;
+//         }
+       
+//       })
+//       return div;
+// })
+// }
 
-updateDiv('c',grids);
+
+let updateDiv = (player, initialBoard) => {
+  initialBoard.forEach((div, index) => {
+    div.addEventListener("click", (e) => {
+      player = switchRounds(player);
+
+      initialBoard[index].textContent = player.marker;
+        if(checkWinner(player.marker)) {
+            resultP.textContent=`Game over! ${player.marker} wins!`;
+            restartGame(initialBoard);
+            return;
+        }
+
+        if(isArrayFull(initialBoard)) {
+          resultP.textContent= `Game is tied!`;
+          return;
+      }
+
+      let activeDiv = e.target;
+      if(activeDiv.textContent === ''){
+        activeDiv.textContent = player.marker;
+      }
+      return activeDiv;
+    })
+})
+
+  const winner = checkWinner(player.marker);
+  console.log(winner);
+
+}
+updateDiv(playerOne,innitialBoard)
+
+
+function restartGame(board) {
+  for(let i = 0; i < board.length; i++) {
+      board[i].textContent = "";
+  }
+  resultP.textContent=`${currentPlayer().activePlayer.marker}'s turn!`
+  // currentPlayer = players[0]
+}
